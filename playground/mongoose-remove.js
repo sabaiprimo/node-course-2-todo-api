e24641a56f16b209575985582bd45ddb9503bfab
Sabai <sabaiprimo@gmail.com>
2019-02-06 23:20:38 +0700

    Add PATCH /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -9,9 +9,9 @@ const {User} = require('../server/models/user');
 // });
 
 // Todo.findOneAndRemove()
-Todo.findOneAndRemove({_id:'5c59b11c25fd42e4b64aaf0d'}).then((todo)=>{
+// Todo.findOneAndRemove({_id:'5c59b11c25fd42e4b64aaf0d'}).then((todo)=>{
 
-});
+// });
 
 Todo.findByIdAndRemove('5c59b11c25fd42e4b64aaf0d').then((todo)=>{
     console.log(todo);
