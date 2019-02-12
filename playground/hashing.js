ee816ce3f7243fa973812655e7e8fb96f7bb9b7c
Sabai <sabaiprimo@gmail.com>
2019-02-12 18:01:50 +0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+//bigger route number more time
+// bcrypt.genSalt(10, (err,salt) =>{
+//     bcrypt.hash(password, salt, (err,hash) =>{
+//         console.log(hash);
+//     });
+// });
 
-var decoded=jwt.verify(token,'123abc');
-console.log('decoded',decoded);
+var hashedPassword = '$2a$10$KIXxHEzpxgZZJQQQWxdoied1IHQfl8bJMsY6HRMfyjTl8OPyXZczu'
+
+bcrypt.compare('123',hashedPassword, (err,res) =>{
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded=jwt.verify(token,'123abc');
+// console.log('decoded',decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -37,5 +53,3 @@ console.log('decoded',decoded);
 //     console.log('Data was changed. Do not trust');
 // }
 
-var hi = SHA256('I love you').toString();
-console.log(hi);
