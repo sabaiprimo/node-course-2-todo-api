896c85429442d387ea399215ecf045e7db553e73
Sabai <sabaiprimo@gmail.com>
2019-02-12 20:14:22 +0700

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -70,8 +70,27 @@ UserSchema.statics.findByToken = function (token) {
         '_id': decoded._id,
         'tokens.token': token,
         'tokens.access': 'auth'
+    });  
+};
+
+UserSchema.statics.findByCredential = function (email, password) {
+    var User = this;
+    
+    return User.findOne({email}).then((user) =>{
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password , user.password, (err,res) =>{
+                if(res){
+                    resolve(user);
+                }else{
+                    reject();
+                }
+            });
+        });
     });
-     
 };
 
 UserSchema.pre('save', function (next) {
--- server/server.js
+++ server/server.js
@@ -108,6 +108,19 @@ app.get('/users/me', authenticate, (req,res)=>{
     res.send(req.user);
 });
 
+app.post('/users/login', (req,res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredential(body.email, body.password).then((user)=>{
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) =>{
+        res.status(400).send();
+        console.log('Unable to login');
+    });
+});
+
 app.listen(port, ()=> {
     console.log(`Started on port ${port}`);
 });
