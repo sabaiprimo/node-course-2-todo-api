26345f917dc3063107f4de7c2cc884727b202330
Sabai <sabaiprimo@gmail.com>
2019-02-12 19:54:05 +0700

    Add test for GET /users/me and POST /users

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -2,7 +2,7 @@ const {MongoClient, ObjectID} = require('mongodb');
 
 
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
+MongoClient.connect('mongodb://127.0.0.1:27017/TodoAppTest', (err, client)=>{
     if(err){
         return console.log('Unable to connect to MongoDb server');
     }
