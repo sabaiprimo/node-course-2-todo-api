fb85e2e06ee38eaaf0435d7399d51e500ea87c6c
Sabai <sabaiprimo@gmail.com>
2019-02-15 16:34:25 +0700

    Add package validator

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,7 @@ UserSchema.methods.generateAuthToken = function () {
     var access = 'auth';
     var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
-    user.tokens = user.tokens.concat([{access , token}]);
+    user.tokens = user.tokens.concat([{access, token}]);
 
     return user.save().then(() =>{
         return token;
